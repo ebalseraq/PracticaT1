2d642ed0cf69b54bbb70184cbfbf9fbaa17be983
encarna <encarna@LAPTOP-GSOHQE2H>
2023-11-27 19:11:05 +0100

    _Añadiendo main

---
+++ MiProyectoPractiaGit/src/miproyectopractiagit/MiProyectoPractiaGit.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package miproyectopractiagit;
+
+/**
+ *
+ * @author encarna
+ */
+public class MiProyectoPractiaGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        System.out.println("HOla");
+    }
+    
+}
